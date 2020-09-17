22868a654f8bba4f0e3074c80e61fc6737c0e9c8
kamran44 <kamranmaqbool37@gmail.com>
2020-09-17 14:33:02 +0500

    add interiaJs & integrate InertiaJS

---
+++ resources/js/Pages/Dashboard/index.vue
@@ -0,0 +1,12 @@
+<template>
+	<div>
+		<h1>Hello Vue Component are here</h1>
+		<p>My Name is {{name}} and my Deggree is {{degree}}</p>
+	</div>
+</template>
+
+<script>
+	export default {
+		props:['name','degree']
+	}
+</script>
